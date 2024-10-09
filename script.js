e32331d305d60647643a91534f3150105b57c41e
haytam <haytam.hadad.2022@gmail.com>
2024-10-09 19:41:38 +0100

    i added some information about the project in readme file

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const apiKey = "7050f6e3f12b4a4794b0ab06803e88e5";
 let currentLanguage = 'en';
 const url = `https://newsapi.org/v2/top-headlines?language=${currentLanguage}&apiKey=${apiKey}`;
 
+
 function fetchNews(fetchUrl) {
   fetch(fetchUrl)
     .then(response => response.json())
