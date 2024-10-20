c7201b6724a610718b524395eed5ca5a782742da
haytam <haytam.hadad.2022@gmail.com>
2024-10-20 14:07:20 +0100

    new version

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -17,7 +17,7 @@ const timeAgo = (publishedDate) => {
   const minutes = Math.floor(diffInSeconds / 60);
   const hours = Math.floor(minutes / 60);
   const days = Math.floor(hours / 24);
-  const months = Math.floor(days / 30.44);
+  const months = Math.floor(days / 30.44); 
   const years = Math.floor(days / 365.25);
 
   if (years > 0) {
