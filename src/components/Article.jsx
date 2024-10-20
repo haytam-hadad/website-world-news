8e09c1d80177c7554c3cf83d13e45e16bc504989
haytam <haytam.hadad.2022@gmail.com>
2024-10-20 12:18:01 +0100

    new look

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -13,33 +13,37 @@ const Article = ({
     e.target.src = "src/assets/favicon.png";
   };
 
-  return (
-    <article>
-      <img
-        src={urlToImage ? urlToImage : "src/assets/favicon.png"}
-        alt={title ? `${title} - News Image` : "News Image"}
-        onError={handleImageError}
-        loading="lazy"
-      />
-      <p className="time">
-        <small>
-          Published : {publishedAt ? publishedAt : "Unknown time"} <i className="fa-regular fa-clock" />
-        </small>
-      </p>
-      <p className="source">{source ? source : "Unknown source"} - source</p>
-      <h2>{title}</h2>
-      <p className='desc'>{description ? description : "No Description"}</p>
-      <p>
-        <strong>
-          By: {author ? author : "Unknown"} <i className="fa-solid fa-feather" />
-        </strong>
-      </p>
-      <hr />
-      <Link className="link_art" to={url ? url : "#" } target="_blank" rel="noopener noreferrer">
-        Read full article:
-      </Link>
-    </article>
-  );
+  if(title != "[Removed]"){
+    return (
+
+      <article>
+        <img
+          src={urlToImage ? urlToImage : "src/assets/favicon.png"}
+          alt={title ? `${title} - News Image` : "News Image"}
+          onError={handleImageError}
+          loading="lazy"
+        />
+        <p className="time">
+          <small>
+            Published : {publishedAt ? publishedAt : "Unknown time"} <i className="fa-regular fa-clock" />
+          </small>
+        </p>
+        <p className="source">{source ? source : "Unknown source"} - source</p>
+        <h2>{title}</h2>
+        <p className='desc'>{description ? description : "No Description"}</p>
+        <p>
+          <strong>
+            By: {author ? author : "Unknown"} <i className="fa-solid fa-feather" />
+          </strong>
+        </p>
+        <hr />
+        <Link className="link_art" to={url ? url : "#" } target="_blank" rel="noopener noreferrer">
+          Read full article:
+        </Link>
+      </article>
+    );    
+  }
+
 };
 
 
