debea3dc776ec66a0105667233b13567bac2e63e
haytam <haytam.hadad.2022@gmail.com>
2024-10-26 11:31:27 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 import Header from "./components/Header";
 import Articles from "./components/Articles";
 import Footer from './components/Footer';
-import { useState } from 'react';
+import { useState  } from 'react';
 import SideMenu from './components/SideMenu';
 import TopSearchBar from './components/TopSearchBar';
 import MainTitle from './components/MainTitle';
-import { app, analytics } from "./firebase";  // Add this line to import Firebase
 
 const apiKey = import.meta.env.VITE_API_KEY;
 const apiUrl = import.meta.env.VITE_API_URL;
+
 const TopSearshsArray = ["russia and ukraine", "isreal and gaza", "morocco news", "USA election"];
+
 
 function App() {
   const [categState, setCategState] = useState('general');
@@ -25,32 +26,20 @@ function App() {
       <Header 
         setCategState={setCategState} 
         setLanguage={setLanguage} 
-        language={language} 
+        language={language}
         search={search} 
         setSearch={setSearch} 
-        setDosearch={setDosearch} 
-        setShowSideMenu={setShowSideMenu} 
-        showInpBox={showInpBox} 
-        setShowInpBox={setShowInpBox} 
+        setDosearch={setDosearch}
+        setShowSideMenu={setShowSideMenu}
+        showInpBox={showInpBox}
+        setShowInpBox={setShowInpBox}
       />
-      <TopSearchBar 
-        TopSearshsArray={TopSearshsArray} 
-        setSearch={setSearch} 
-        setDosearch={setDosearch} 
-        setShowInpBox={setShowInpBox}  
-      />
+      <TopSearchBar TopSearshsArray={TopSearshsArray} setSearch={setSearch} setDosearch={setDosearch} setShowInpBox={setShowInpBox}  />
       <MainTitle title={"Today's Top News Headlines"}/>
       { showSideMenu ?  <SideMenu setCategState={setCategState}/> : null }
-      <Articles 
-        apiUrl={apiUrl} 
-        apiKey={apiKey} 
-        categState={categState} 
-        language={language} 
-        dosearch={dosearch} 
-        setDosearch={setDosearch} 
-        search={search}
-      />
-      <Footer/>
+      <Articles apiUrl={apiUrl} apiKey={apiKey} categState={categState} language={language}
+        dosearch={dosearch} setDosearch={setDosearch} search={search}/>
+        <Footer/>
     </React.Fragment>
   );
 }
