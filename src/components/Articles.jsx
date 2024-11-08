c1b859a00fba3881324ea2c9c98f858383689d3a
haytam <haytam.hadad.2022@gmail.com>
2024-11-08 11:08:00 +0100

    .

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -48,7 +48,7 @@ const Articles = ({ categState, language, dosearch, setDosearch, search }) => {
       { categState === "general" && <MainTitle title={"Today's Top News Headlines"}/> }
       <main>
         {loading ? (
-          <ClipLoader className="spinner" color={"#fff"} loading={true} size={40} />
+          <ClipLoader className="spinner" color={"#000"} loading={true} size={45} />
         ) : articles.length > 0 ? (
           articles.map((article, index) => (
             <Article
