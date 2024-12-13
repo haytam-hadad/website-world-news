7363f6a5138a0da5acaf599ec5eda4e787fec212
haytam <haytam.hadad.2022@gmail.com>
2024-12-13 18:34:43 +0100

    .

--- app/components/Article.js
+++ app/components/Article.js
@@ -1,11 +1,11 @@
 import Link from "next/link";
 import Image from "next/image";
-import { Clock3 } from 'lucide-react';
+import { Clock3 } from "lucide-react";
 
 const Article = ({ title, desc }) => {
   return (
     <div className="flex hover:shadow-md flex-col border shadow-sm flex-1 p-1 gap-1 min-w-[300px] rounded-xl overflow-hidden">
-      <div className="rounded-lg overflow-hidden w-full h-fit">
+      <div className="rounded-xl overflow-hidden w-full h-fit">
         <Image
           className="w-full max-h-50 object-cover"
           src="/images/image.jpg"
@@ -13,27 +13,29 @@ const Article = ({ title, desc }) => {
           width={400}
           height={200}
         />
+        <div className="flex items-center justify-between bg-thirdColor p-2 text-sm text-secondaryColor dark:bg-secondaryColor dark:text-mainTextColor font-semibold">
+          <Link
+            className="cursor-pointer rounded-xl hover:underline flex items-center"
+            href="https://www.bbc.com/news"
+            target="_blank"
+          >
+            <Image
+              className="rounded-full w-7 h-7 mr-3 outline outline-2 outline-mainColor outline-offset-2"
+              src="/images/image.jpg"
+              alt="www"
+              width={400}
+              height={200}
+            />
+            BBC - news
+          </Link>
+          <span className="separator">|</span>
+          <p className="text-[gray] flex items-center">
+            2 hours ago <Clock3 className="mx-2 h-4 w-3" />{" "}
+          </p>
+        </div>
       </div>
 
-      <div className="flex items-center justify-between bg-thirdColor p-2 text-secondaryColor dark:bg-secondaryColor dark:text-mainTextColor rounded-lg font-semibold">
-        <Link
-          className="cursor-pointer rounded-xl hover:underline flex items-center"
-          href="https://www.bbc.com/news"
-          target="_blank"
-        >
-          <Image
-            className="rounded-full w-7 h-7 mr-3 outline outline-2 outline-mainColor outline-offset-2"
-            src="/images/image.jpg"
-            alt="www"
-            width={400}
-            height={200}
-          />
-          BBC - news
-        </Link> 
-        <span className="separator"> | </span>
-        <p className="text-[gray] flex items-center">2 hours ago <Clock3 className="mx-1 h-4 w-3" /> </p>
-      </div>
-      <h1 className="font-bold font-serif text-3xl p-3 underline ">{title}</h1>
+      <h1 className="font-bold font-serif text-3xl py-2 px-3 underline ">{title}</h1>
       <p className="text-md  px-3 py-1 flex-grow">{desc}</p>
       <div className="flex p-1 justify-between items-center mt-3">
         <Link
