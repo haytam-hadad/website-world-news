e97271e98082a1e13438a7adeb56661f5605d8d7
haytam <haytam.hadad.2022@gmail.com>
2025-02-09 13:55:53 +0100

    add no articles message to Search component and update Article props for better content handling

--- app/search/[query]/page.js
+++ app/search/[query]/page.js
@@ -45,16 +45,23 @@ const Search = async ({ params }) => {
         </div>
       )}
 
-      <main className="flex flex-wrap justify-center sm:justify-start md:justify-around gap-2">
-        {articles.length > 0 &&
+<main className="flex flex-wrap justify-center sm:justify-start md:justify-around gap-2">
+        {articles.length === 0 ? (
+          <div className="no_articles_container">
+            <h1 className="no_articles">
+              No articles in this category &quot;<b>{category}</b>&quot; available
+            </h1>
+          </div>
+        ) : (
           articles.map((article) => {
             const {
               _id,
               title = "No title available",
-              description = "No description available", // Ensure description is being used here
+              content = "No content available",
               imageUrl = "/images/default.jpg",
               author = "Unknown",
               publishedAt,
+              category = "General",
               url = "#",
             } = article || {};
 
@@ -62,14 +69,16 @@ const Search = async ({ params }) => {
               <Article
                 key={_id || Math.random().toString(36)}
                 title={title}
-                desc={description} // Set desc to 'description' here
+                desc={content}
                 imageUrl={imageUrl}
                 author={author}
                 publishedAt={publishedAt && !isNaN(new Date(publishedAt)) ? new Date(publishedAt).toLocaleString() : "N/A"}
+                category={category}
                 url={url}
               />
             );
-          })}
+          })
+        )}
       </main>
     </div>
   );
