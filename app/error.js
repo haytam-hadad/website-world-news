ae54031d11fcb849b0fd9cfe25a6cc8902495126
haytam <haytam.hadad.2022@gmail.com>
2025-02-19 20:53:09 +0100

    update ErrorComponent; add close button functionality and manage visibility state for improved user experience

--- app/error.js
+++ app/error.js
@@ -1,17 +1,34 @@
 "use client";
 
-import React from 'react';
+import React, { useState } from 'react';
 
 const ErrorComponent = ({ message }) => {
+  const [isVisible, setIsVisible] = useState(true);
+
+  if (!isVisible) return null;
+
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-100">
       <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
         <strong className="font-bold">Error:</strong>
         <span className="block sm:inline">{message}</span>
+        <span 
+          className="absolute top-0 bottom-0 right-0 px-4 py-3" 
+          onClick={() => setIsVisible(false)}
+        >
+          <svg 
+            className="fill-current h-6 w-6 text-red-500" 
+            role="button" 
+            xmlns="http://www.w3.org/2000/svg" 
+            viewBox="0 0 20 20"
+          >
+            <title>Close</title>
+            <path d="M14.348 5.652a1 1 0 10-1.414-1.414L10 7.172 7.066 4.238a1 1 0 10-1.414 1.414L8.828 10l-3.176 3.176a1 1 0 101.414 1.414L10 12.828l2.934 2.934a1 1 0 001.414-1.414L11.172 10l3.176-3.176z"/>
+          </svg>
+        </span>
       </div>
     </div>
   );
 };
 
 export default ErrorComponent;
-
