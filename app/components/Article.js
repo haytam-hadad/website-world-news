1075d6462810aa29a495b15cac81074a21ee8539
haytam <haytam.hadad.2022@gmail.com>
2025-02-19 20:14:15 +0100

    update Article and OnePost components; enhance category link styling for improved user interaction

--- app/components/Article.js
+++ app/components/Article.js
@@ -68,7 +68,7 @@ const Article = ({ title, desc, imageUrl, author, publishedAt, category, url }) 
             <div className="flex flex-col justify-between w-full">
               <header className="flex items-center justify-between text-sm text-gray-500 dark:text-gray-400 mb-2 p-1">
                 <div className="flex items-center space-x-2">
-                  <Link href={`/profile/${author}`} className="rounded-full cursor-pointer border-mainColor bg-gray-300 dark:bg-gray-700 w-10 h-10 flex items-center justify-center text-white font-bold">
+                  <Link href={`/profile/${author}`} className="rounded-full cursor-pointer border-mainColor bg-gray-400 dark:bg-gray-800 w-10 h-10 flex items-center justify-center text-white font-bold">
                     {author ? author[0].toUpperCase() : "U"}
                   </Link>
                   <div className="flex flex-col">
@@ -93,7 +93,7 @@ const Article = ({ title, desc, imageUrl, author, publishedAt, category, url }) 
                   {subscribed ? "Unsubscribe" : "Subscribe"}
                 </button>
               </header>
-              <hr className="mx-5"/>
+
               <h1 className="font-serif font-semibold text-2xl text-primary underline underline-offset-4 sm:text-2xl mx-1 my-5 capitalize text-gray-900">
                 {title}
               </h1>
@@ -134,9 +134,9 @@ const Article = ({ title, desc, imageUrl, author, publishedAt, category, url }) 
                   </div>
                 </div>
 
-                <div className="flex items-center space-x-6 cursor-pointer text-gray-500 hover:text-blue-500">
-                  <p className="text-blue-500 cursor-pointer font-semibold capitalize text-md p-1">{category || "General"}</p>
-                </div>
+                <Link href={`/category/${category}`} className="flex items-center space-x-6 cursor-pointer text-gray-500 hover:text-blue-500">
+                  <p className="text-blue-500 cursor-pointer font-semibold capitalize text-md py-1 transition duration-300 px-2 hover:border border-transparent hover:border-mainColor rounded-full">{category || "General"}</p>
+                </Link>
               </div>
             </div>
           </div>
--- app/components/OnePost.js
+++ app/components/OnePost.js
@@ -150,11 +150,9 @@ const SinglePost = ({ post }) => {
               <MoreHorizontal className="w-5 h-5 transition-transform transform hover:scale-110" />
             </div>
           </div>
-          <div className="flex items-center space-x-6 cursor-pointer text-gray-500 hover:text-blue-500">
-            <p className="text-blue-500 cursor-pointer font-semibold capitalize text-md p-2">
-              {post.category || "General"}
-            </p>
-          </div>
+          <Link href={`/category/${post.category}`} className="flex items-center space-x-4 cursor-pointer text-lg text-gray-500 hover:text-blue-500">
+              <p className="text-blue-500 cursor-pointer font-semibold capitalize text-md p-1 px-3 transition duration-300 hover:border border hover:border-mainColor rounded-full">{post.category || "General"}</p>
+          </Link>
         </div>
       </div>
 
